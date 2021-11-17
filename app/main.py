2b784faf25525e64a5b4b04b2cb20e9e486a962c
Dries Augustyns <augustyns.dries@gmail.com>
2021-11-17 13:25:57 +0100

    Push

--- app/main.py
+++ app/main.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import random
+import os
+from ml import get_waldo_bounds
 
-app = Flask(__name__)
+app = Flask(__name__,  static_url_path='/static')
 
 
 @app.route("/")
 def index():
-    return "Test"
+    random_image = random.choice(os.listdir('./static'))
+
+    bounds = get_waldo_bounds(random_image)
+    print(bounds)
+    return render_template('index.html', image=random_image)
+
+
+@app.route("/click", methods=['POST'])
+def click():
+    data = request.json()
+    print(data)
 
 
 app.run()
