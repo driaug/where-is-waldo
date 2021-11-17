0f3dbb841b833705ada48fdb38b30be39a8e5438
Dries Augustyns <augustyns.dries@gmail.com>
2021-11-17 14:31:35 +0100

    Added routes

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import random
 import os
 from ml import get_waldo_bounds
@@ -6,19 +6,18 @@ from ml import get_waldo_bounds
 app = Flask(__name__,  static_url_path='/static')
 
 
-@app.route("/")
+@app.route("/click", methods=['POST'])
+def click():
+    data = request.get_json()
+
+    return jsonify(data)
+
+
+@app.route("/", methods=['GET'])
 def index():
     random_image = random.choice(os.listdir('./static'))
 
-    bounds = get_waldo_bounds(random_image)
-    print(bounds)
     return render_template('index.html', image=random_image)
-
-
-@app.route("/click", methods=['POST'])
-def click():
-    data = request.json()
-    print(data)
 
 
 app.run()
