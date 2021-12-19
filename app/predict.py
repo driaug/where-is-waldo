631a92559fc8bffc3b0e88c58bc798e0995f3e86
Niels baptist <r0793266@student.thomasmore.be>
2021-12-19 14:24:31 +0100

    changed to resnet

--- app/predict.py
+++ app/predict.py
@@ -7,7 +7,7 @@ def predict(image):
     model = load_learner('../notebook/export.pkl')
     prediction = model.predict(image)
     print(str(prediction[2][1] * 100) + '%')
-    if prediction[2][1] > 0.9952:
+    if prediction[2][1] > 0.95:
         return prediction[0]
     else:
         print("DARN")
